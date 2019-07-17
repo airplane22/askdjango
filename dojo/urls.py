e91cbf5e1a778e70c5f2918337815621b89c02d8
soyun22 <2002airplane4@gmail.com>
2019-07-17 23:54:15 +0900

    dojo.views.hello : 한글 문자열 매칭

--- dojo/urls.py
+++ dojo/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum)
+    re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+    #path('hello/<str:name>/<int:age>/', views.hello), #repath로 ㄱ-힣으로 name 한글 문자열 매칭으로 바꾸기!!
+    re_path(r'^hello/(?P<name>[ㄱ-힣/]+)/(?P<age>[\d]+)/$', views.hello),
 
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -8,3 +8,6 @@ def mysum(request, numbers):
     #numbers = "1/12/123/12/123"
     result = sum(map(lambda s : int(s or 0), numbers.split("/")))
     return HttpResponse(result)
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name, age))
