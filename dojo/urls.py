f14c7f38bf6b8b979b5a2d03f11438c0cbaf4d09
soyun22 <2002airplane4@gmail.com>
2019-07-18 00:52:07 +0900

    함수 기반 뷰(FBV)의 4가지 패턴

--- dojo/urls.py
+++ dojo/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     re_path(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     #path('hello/<str:name>/<int:age>/', views.hello), #repath로 ㄱ-힣으로 name 한글 문자열 매칭으로 바꾸기!!
     re_path(r'^hello/(?P<name>[ㄱ-힣/]+)/(?P<age>[\d]+)/$', views.hello),
+    path('post_list1/', views.post_list1),
+    path('post_list2/', views.post_list2),
+    path('post_list3/', views.post_list3),
+    path('excel/', views.excel_download),
 
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from django.conf import settings
+import os
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 
 def mysum(request, numbers):
     #numbers = "1/12/123/12/123"
@@ -11,3 +12,30 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요. {}. {}살이시네요'.format(name, age))
+
+def post_list1(request):
+    name = '공유'
+    return HttpResponse('''
+        <h1>AskDjango</h1>
+        <p>{name}</p>
+        <p>여러분의 파이썬&장고 페이스메이커</p>
+    '''.format(name=name))
+
+def post_list2(request):
+    name = '공유'
+    return  render(request, 'dojo/post_list.html', {'name': name})
+
+def post_list3(request):
+    return JsonResponse({
+    'message':'안녕 파이썬 장고',
+    'items' : ['파이썬', '장고','Celery','Azure','Aws']
+    }, json_dumps_params={'ensure_ascii':False})
+
+def excel_download(request):
+    # filepath = 'c:\dev\djbasic_practice.xlsx'
+    filepath = os.path.join(settings.BASE_DIR, 'djbasic_practice.xlsx')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename = "{}"'.format(filename)
+        return response
