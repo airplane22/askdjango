831e7c191dff9bcac23dbe6326323446d63c85b5
soyun22 <2002airplane4@gmail.com>
2019-07-23 10:39:25 +0900

    프로젝트 템플릿에 bootstrap3 스타일을 적용

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@
 
 from django.urls import path
 from . import views
+from . import views_cbv
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<id>', views.post_detail, name='post_detail')
+    path('<id>', views.post_detail, name='post_detail'),
+
+    path('cbv/new/', views_cbv.post_new)
 ]
