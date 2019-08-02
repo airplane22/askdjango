951c895deead0eb08033dad3e1f7138581c3baff
soyun22 <2002airplane4@gmail.com>
2019-08-03 02:35:50 +0900

    imagekit을 활용하여, 이미지가 업로드될 때, 썸네일 처리된 이미지만 저장

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ from django.db import models
 from django.forms import ValidationError
 # Create your models here.
 from django.urls import reverse
-from imagekit.models import ImageSpecField
+# from imagekit.models import ImageSpecField
+from imagekit.models import ProcessedImageField
 from imagekit.processors import Thumbnail
 
 
@@ -28,12 +29,15 @@ class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text='포스팅 제목을 입력해주세요.(100자 내외)')
     content = models.TextField(verbose_name='내용')            #길이 제한이 없는 문자열-Textfield
-    photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
-    photo_thumbnail = ImageSpecField(source='photo',
-                                     processors=[Thumbnail(300, 300)],
-                                     format='JPEG',
-                                     options={'quality':60})
-
+    # photo = models.ImageField(blank=True, upload_to='blog/post/%Y/%m/%d')
+    # photo_thumbnail = ImageSpecField(source='photo',
+    #                                  processors=[Thumbnail(300, 300)],
+    #                                  format='JPEG',
+    #                                  options={'quality':60})
+    photo = ProcessedImageField(blank=True, upload_to='blog/post/%Y/%m/%d',
+                                processors=[Thumbnail(300,300)],
+                                format='JPEG',
+                                options={'quality':60})
     tags = models.CharField(max_length=100, blank=True)
     lnglat = models.CharField(max_length=50, blank=True,
                               validators=[lnglat_validator], help_text='경도,위도 포맷으로 입력')
