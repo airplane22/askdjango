e1272f98dfc9752e9cdd32b45fb0bccf5b33b51c
soyun22 <2002airplane4@gmail.com>
2019-08-03 00:49:07 +0900

    messages framework 의 level/tags 설정 변경

--- blog/views.py
+++ blog/views.py
@@ -10,13 +10,10 @@ from django.contrib import messages
 def post_list(request):
     qs = Post.objects.all() #아직 DB 접속 이루어지지 않음
 
-
-
 ##q 인자 가져오는법 다시보기!!!
     q = request.GET.get('q', '')
     if q:
         qs = qs.filter(title__icontains=q)
-
     return render(request, 'blog/post_list.html',{
         'post_list':qs,
         # 'q'=q, ??? 9강 25:00
--- project_djbasic/settings.py
+++ project_djbasic/settings.py
@@ -133,3 +133,9 @@ STATIC_URL = '/static/'
 
 
 INTERNAL_IPS = ['127.0.0.1']
+
+
+from django.contrib.messages import constants
+
+MESSAGE_LEVEL = constants.DEBUG
+MESSAGE_TAGS = {constants.ERROR:'danger'}
